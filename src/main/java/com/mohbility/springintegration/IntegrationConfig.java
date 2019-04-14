013d0cf342c0c4df832ee80e81454fb68846d097
mohbility <mohbility@gmail.com>
2019-04-14 00:42:26 -0400

    Added all the files and test it

---
+++ src/main/java/com/mohbility/springintegration/IntegrationConfig.java
@@ -0,0 +1,51 @@
+package com.mohbility.springintegration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
+
+import java.io.File;
+
+@Configuration
+public class IntegrationConfig {
+
+
+    private Transformer transformer;
+
+    public IntegrationConfig(Transformer transformer) {
+        this.transformer = transformer;
+    }
+
+    @Bean
+    public IntegrationFlow integrationFlow(){
+
+        return IntegrationFlows.from(fileReader(),
+                spec -> spec.poller(Pollers.fixedDelay(500)))
+                .transform(transformer,"transform")
+                .handle(fileWriter())
+                .get();
+    }
+
+    @Bean
+    public FileWritingMessageHandler fileWriter() {
+
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(
+                new File("destination")
+        );
+        handler.setExpectReply(false);
+        return handler;
+    }
+
+    @Bean
+    public FileReadingMessageSource fileReader() {
+
+        FileReadingMessageSource source = new FileReadingMessageSource();
+        source.setDirectory(new File("source"));
+
+        return source;
+    }
+}
